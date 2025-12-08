273c84520bab65930e6b0c2e6d20084c660d59cf
Abhishek4597 <abhishekdandwate4597@gmail.com>
2025-12-08 22:04:42 +0530

    initial commit

---
+++ src/main/java/com/sahyadri_multitradelinks/SahyadriMultitradelinksApplication.java
@@ -0,0 +1,13 @@
+package com.sahyadri_multitradelinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SahyadriMultitradelinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SahyadriMultitradelinksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sahyadri_multitradelinks/SahyadriMultitradelinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahyadri_multitradelinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SahyadriMultitradelinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
